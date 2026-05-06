457ab59b161ad17aae8983db30a32a1fc7fee546
David <davidfajardo727@gmail.com>
2026-05-05 22:44:10 -0500

    "Primer Commit: creación de clases y métodos básicos"

---
+++ src/main/java/App/GuiaPractica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package App;
+
+/**
+ *
+ * @author User
+ */
+public class GuiaPractica {
+
+    public static void main(String[] args) {
+        
+    }
+}
